6b7fc0f4a652ac5e2b5557e6b9394076b5306748
Hugo Maldonado <Hugo Maldonado>
2021-10-16 13:19:32 -0300

    se termina de construir la clase pelicula y se vuelve a crear un nuevo paquete

--- src/mx/com/gm/peliculas/domain/Pelicula.java
+++ src/mx/com/gm/peliculas/domain/Pelicula.java
@@ -2,5 +2,32 @@
 package mx.com.gm.peliculas.domain;
 
 public class Pelicula {
+    private String nombre;
+    
+    //constructor vacio
+    public Pelicula(){
+        
+    }
+    //constructor con un argumento que inicializa el nombre de la pelicula
+    public Pelicula(String nombre){
+        this.nombre = nombre;
+    }
+    //metodo que muestra el nombre
+    public String getNombre() {
+        return nombre;
+    }
+    //metodo que modifica el nombre
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Pelicula{nombre=").append(nombre);
+        sb.append('}');
+        return sb.toString();
+    }
+    
     
 }
